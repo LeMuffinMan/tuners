e251ee7f56b401c4c1e3627eafd6ec9131782934
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-23 19:54:47 +0100

    basic volume visualisation

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use cpal::traits::HostTrait;
+use cpal::traits::StreamTrait;
+use cpal::traits::DeviceTrait;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+
+fn main() {
+    let host = cpal::default_host();
+    let device = host
+        .default_input_device()
+        .expect("no input device available");
+    let config = device.default_input_config().expect("no default config");
+
+    // We NEVER want to interrupt the call back, so we need a shared buffer with a mutex to allow
+    // another thread to operate while the main one is busy with continuous audio input callback
+
+    let shared_buffer = Arc::new(Mutex::new(Vec::<f32>::new()));
+    let buffer_for_audio = Arc::clone(&shared_buffer);
+    let device_name = device.name().unwrap_or_else(|_| "unknown device".to_string());
+
+    println!("Using device '{}' with config: {:?}", device_name, config);
+
+    let input_stream = device.build_input_stream(
+        &config.into(),
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            // println!("received {} samples", data.len());
+            let mut buf = buffer_for_audio.lock().expect("error locking buffer_for_audio mutex");
+
+            //since data is a slice [], we can't push &[T] in buffer, we need extend_from_slice 
+            buf.extend_from_slice(data); 
+            let len = buf.len();
+            if len > 4096 {
+                let limit = len - 4096;
+                //this delete the oldests samples to not grow in RAM
+                buf.drain(0..limit);
+            }
+        },
+        move |err| {
+            println!("there was an error : {err}");
+        },
+        None, // Some(Duration) = timeout
+    ).expect("failed to build input stream");
+
+    input_stream.play().expect("failed to play stream");
+
+    loop {
+        std::thread::sleep(Duration::from_millis(100));
+
+        let buffer = shared_buffer.lock().expect("error locking shared_buffer mutex");
+        if buffer.is_empty() {
+            continue;
+        }
+
+        let rms = (buffer.iter().map(|x| x * x).sum::<f32>() / buffer.len() as f32).sqrt();
+
+        let bars = (rms * 50.0) as usize;
+        println!("{: <50}", "█".repeat(bars));
+    }
+}
