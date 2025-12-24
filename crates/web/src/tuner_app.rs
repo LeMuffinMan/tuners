fa10459270b9d9b775b2e880707f29d4397f6dbe
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-24 23:25:23 +0100

    reorganized web/lib.rs

---
+++ crates/web/src/tuner_app.rs
@@ -0,0 +1,145 @@
+use crate::AudioRingBuffer;
+use crate::start_audio;
+use crate::take_ringbuffer;
+use egui::{ Rect, Pos2, Color32, Vec2 };
+
+pub enum UiType {
+    Mobile,
+    Desktop,
+}
+
+enum Visualizer {
+    Freq,
+    RMS,
+    WaveShape,
+}
+
+pub struct TunerApp {
+    ui_type : UiType, 
+    visualizer: Visualizer,
+    ringbuff: Option<AudioRingBuffer>,
+    audio_start: bool,
+}
+
+impl TunerApp {
+    pub fn new(ui_type: UiType) -> Self {
+        Self {
+            ui_type,
+            ringbuff: None,
+            visualizer: Visualizer::RMS,
+            audio_start: false,
+        }
+    }
+
+    pub fn start_audio(&mut self) {
+        if self.audio_start {
+            return;
+        }
+
+        self.audio_start = true;
+
+        wasm_bindgen_futures::spawn_local(async {
+            start_audio().await.unwrap();
+        });
+    }
+}
+
+impl TunerApp {
+    fn get_rms(&mut self) -> f32 {
+        if let Some(ringbuff) = &mut self.ringbuff {
+            let n = ringbuff.len();
+            if n == 0 {
+                return 0.0;
+            }
+
+            let mut tmp = vec![0.0; n];
+            let read = ringbuff.pop_block(&mut tmp);
+            if read == 0 {
+                return 0.0;
+            }
+            (tmp[..read].iter().map(|x| x * x).sum::<f32>() / ringbuff.len() as f32).sqrt()
+        } else {
+            0.0
+        }
+    }
+
+    fn render_rms(&mut self, ui: &mut egui::Ui) {
+        let size = Vec2::new(24.0, 140.0);
+        let (rect, _) = ui.allocate_exact_size(size, egui::Sense::hover());
+        let painter = ui.painter();
+        
+        // fond gris
+        painter.rect_filled(rect, 4.0, Color32::from_gray(30));
+
+        let rms = self.get_rms();
+        let db = rms_to_db(rms);
+        let min_db = -60.0;
+        let max_db = 0.0;
+
+        let norm = ((db - min_db) / (max_db - min_db)).clamp(0.0, 1.0);
+        let fill_height = rect.height() * norm;
+
+        // barre remplie
+        let filled_rect = Rect::from_min_max(
+            Pos2::new(rect.left(), rect.bottom() - fill_height),
+            Pos2::new(rect.right(), rect.bottom()),
+        );
+        painter.rect_filled(filled_rect, 4.0, Color32::from_rgb(0, 200, 0));
+    }
+
+}
+
+impl eframe::App for TunerApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.set_visuals(egui::Visuals::dark());
+        if self.ringbuff.is_none() {
+            self.ringbuff = take_ringbuffer();
+        }
+        egui::CentralPanel::default().show(ctx, |ui| {
+
+            // ui.heading("🎵 Tuner WASM");
+            match &self.ui_type {
+                _ => {
+                    egui::SidePanel::right("mode").show(ctx, |ui| {
+
+
+                        if self.audio_start == false {
+                            if ui.button("🎤 Activer le micro").clicked() {
+                                self.start_audio();
+                            }
+                        }
+
+
+                        if ui.button("RMS").clicked() {
+                            self.visualizer = Visualizer::RMS;
+                        }    
+                        if ui.button("Freq").clicked() {
+                            self.visualizer = Visualizer::Freq;
+                        }    
+                        if ui.button("WaveShape").clicked() {
+                            self.visualizer = Visualizer::WaveShape;
+                        }    
+                    });
+                }
+            }
+            match self.visualizer {
+                _ => {
+                    self.render_rms(ui);
+                }
+                // RMS => {
+                // },
+                // Freq => {
+                //
+                // },
+                // WaveShape => {
+                //
+                // },
+            }
+        });
+    }
+}
+
+fn rms_to_db(rms: f32) -> f32 {
+    20.0 * (rms.max(1e-9)).log10()
+}
+
