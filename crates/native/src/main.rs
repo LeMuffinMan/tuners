27c0a1652ed974cbc0c5a4cb4a563aca76726c44
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-12-24 02:20:09 +0100

    base for native and wasm compilation

--- crates/native/src/main.rs
+++ crates/native/src/main.rs
@@ -4,6 +4,10 @@ use cpal::traits::DeviceTrait;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
+use tuner_dsp::{autocorrelation, freq_to_tune};
+
+//compile from tuners/ with cargo run -p tuners_native
+
 fn main() {
     let host = cpal::default_host();
     let device = host
@@ -84,44 +88,44 @@ fn main() {
             }
         }
     }
-
-
-    fn freq_to_tune(freq: f32) -> String {
-        let tunes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-        let a4 = 440.0;
-
-        let t = (12.0 * (freq / a4).log2()).round() as i32;
-        let tune_index = (t + 9).rem_euclid(12);
-        let octave = 4 + ((t + 9) / 12);
-
-        format!("{}{}", tunes[tune_index as usize], octave)
-    }
-
-    fn autocorrelation(buffer: &[f32], sample_rate: f32) -> Option<f32> {
-        let min_freq = 80.0;
-        let max_freq = 1000.0;
-
-        let min_lag = (sample_rate / max_freq) as usize;
-        let max_lag = (sample_rate / min_freq) as usize;
-
-        let mut best_lag = 0;
-        let mut best_corr = 0.0;
-
-        for lag in min_lag..max_lag {
-            let mut corr = 0.0;
-
-            for i in 0..(buffer.len() - lag) {
-                corr += buffer[i] * buffer[i + lag];
-            }
-            if corr > best_corr {
-                best_corr = corr;
-                best_lag = lag;
-            }
-        }
-        if best_lag > 0 {
-            Some(sample_rate / best_lag as f32)
-        } else {
-            None
-        }
-    }
+    //
+    //
+    // fn freq_to_tune(freq: f32) -> String {
+    //     let tunes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    //     let a4 = 440.0;
+    //
+    //     let t = (12.0 * (freq / a4).log2()).round() as i32;
+    //     let tune_index = (t + 9).rem_euclid(12);
+    //     let octave = 4 + ((t + 9) / 12);
+    //
+    //     format!("{}{}", tunes[tune_index as usize], octave)
+    // }
+    //
+    // fn autocorrelation(buffer: &[f32], sample_rate: f32) -> Option<f32> {
+    //     let min_freq = 80.0;
+    //     let max_freq = 1000.0;
+    //
+    //     let min_lag = (sample_rate / max_freq) as usize;
+    //     let max_lag = (sample_rate / min_freq) as usize;
+    //
+    //     let mut best_lag = 0;
+    //     let mut best_corr = 0.0;
+    //
+    //     for lag in min_lag..max_lag {
+    //         let mut corr = 0.0;
+    //
+    //         for i in 0..(buffer.len() - lag) {
+    //             corr += buffer[i] * buffer[i + lag];
+    //         }
+    //         if corr > best_corr {
+    //             best_corr = corr;
+    //             best_lag = lag;
+    //         }
+    //     }
+    //     if best_lag > 0 {
+    //         Some(sample_rate / best_lag as f32)
+    //     } else {
+    //         None
+    //     }
+    // }
 }
---
+++ crates/web/src/lib.rs
@@ -0,0 +1,37 @@
+use eframe::{App, egui};
+use tuner_dsp::{autocorrelation, freq_to_tune};
+use wasm_bindgen::JsCast;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub async fn start() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("the_canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()?;
+
+    let runner = eframe::WebRunner::new();
+    runner
+        .start(
+            canvas,
+            eframe::WebOptions::default(),
+            Box::new(|_cc| Ok(Box::new(TunerApp::default()) as Box<dyn App>)),
+        )
+        .await
+}
+
+#[derive(Default)]
+struct TunerApp;
+
+impl eframe::App for TunerApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        ctx.set_visuals(egui::Visuals::dark());
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("🎵 Tuner WASM");
+        });
+    }
+}
