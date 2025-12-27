61f567912844598efa5086b80473dbbec4ebdfea
muffin <muffin@arch.man>
2025-12-27 19:45:47 +0100

    documentation and comment deletion in audio

--- crates/audio/src/audio_bridge.rs
+++ crates/audio/src/audio_bridge.rs
@@ -4,11 +4,6 @@ use rtrb::{RingBuffer, Consumer, Producer};
 pub const SAMPLE_RATE: u32 = 44100;
 pub const BUFFER_SIZE: usize = 4096;
 
-pub trait AudioBackend {
-    fn start(&mut self);
-    fn stop(&mut self);
-}
-
 pub struct AudioBridge {
     pub consumer: Consumer<f32>,
 }
