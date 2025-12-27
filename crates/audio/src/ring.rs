76634c4611405d988244d192f816a48d08756a54
muffin <muffin@arch.man>
2025-12-27 17:12:54 +0100

    wasm en native working

--- crates/audio/src/ring.rs
+++ crates/audio/src/ring.rs
@@ -15,53 +15,7 @@ pub struct AudioBridge {
 
 impl AudioBridge {
     pub fn new(buffer_capacity: usize) -> (Self, Producer<f32>) {
-        let (mut producer, mut consumer) = RingBuffer::new(buffer_capacity);
+        let (producer, consumer) = RingBuffer::new(buffer_capacity);
         (Self { consumer }, producer)
     }
 }
-
-
-//
-//
-//
-// use rtrb::{RingBuffer, Producer, Consumer};
-//
-// pub struct AudioRingBuffer {
-//     producer: Producer<f32>,
-//     consumer: Consumer<f32>,
-//     buffer: Vec<f32>, // buffer temporaire pour UI
-// }
-//
-// impl AudioRingBuffer {
-//     pub fn new(size: usize) -> Self {
-//         let (producer, consumer) = RingBuffer::<f32>::new(size);
-//         Self { producer, consumer, buffer: Vec::new() }
-//     }
-//
-//     pub fn push(&mut self, sample: f32) {
-//         let _ = self.producer.push(sample);
-//     }
-//
-//     pub fn read_all(&mut self) -> &[f32] {
-//         self.buffer.clear();
-//         while let Ok(s) = self.consumer.pop() {
-//             self.buffer.push(s);
-//         }
-//         &self.buffer
-//     }
-//
-//     pub fn rms(&self) -> f32 {
-//         if self.buffer.is_empty() { return 0.0; }
-//         let sum: f32 = self.buffer.iter().map(|x| x*x).sum();
-//         (sum / self.buffer.len() as f32).sqrt()
-//     }
-//
-//     pub fn producer(&mut self) -> &mut Producer<f32> {
-//         &mut self.producer
-//     }
-//
-//     pub fn consumer(&mut self) -> &mut Consumer<f32> {
-//         &mut self.consumer
-//     }
-// }
-//
--- crates/native_gui/src/main.rs
+++ crates/native_gui/src/main.rs
@@ -1,20 +1,13 @@
-use cpal::traits::HostTrait;
-use cpal::traits::StreamTrait;
-use cpal::traits::DeviceTrait;
-use audio::backend::native::start_audio_native;
 
 use gui::{UiType, TunerApp};
-
-use tuner_dsp::{autocorrelation, freq_to_tune};
+// use tuner_dsp::{autocorrelation, freq_to_tune};
 
 //compile from tuners/ with cargo run -p tuners_native_gui
 
 fn main() {
-    start_audio_native();
-
     let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Tuner WASM",
+    let _ = eframe::run_native(
+        "Tuner",
         options,
         Box::new(|_cc| Ok(Box::new(TunerApp::new(UiType::Desktop)))),
     );
